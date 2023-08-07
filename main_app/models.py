a1a0d85294f815fc278a78322ecf7f2a4c5ab4f4
cango91 <can.gologlu@gmail.com>
2023-08-07 18:23:57 -0400

    part 2

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Create your models here.
+
+
+class Cat(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    age = models.IntegerField(validators=[MinValueValidator(0)])
+    
+    def __str__(self):
+        return f"{self.id}: {self.name}, {self.breed}, {self.age} y.o."
+    
--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,23 @@
-from django.shortcuts import render
-
-cats = [
-  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
-  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
-]
+from django.shortcuts import render, get_object_or_404
+from .models import Cat
 
 # Create your views here.
+
+
 def home(request):
-    return render(request,'home.html')
+    return render(request, 'home.html')
+
 
 def about(request):
     return render(request, 'about.html')
 
+
 def cats_index(request):
     return render(request, 'cats/index.html', {
-        'cats': cats,
+        'cats': Cat.objects.all(),
     })
+
+
+def cats_detail(request, cat_id):
+    cat = get_object_or_404(Cat, id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
